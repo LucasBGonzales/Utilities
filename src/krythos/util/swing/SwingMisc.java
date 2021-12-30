de9dfe2440c7b6b031ae8304852e299fe876268f
Krythos <lucasbgonzales@gmail.com>
2021-12-30 11:58:10 -0500

    OS Detection Windows & Linux
    added getOS(), isWindows(), and isLinux() functions

---
+++ src/krythos/util/swing/SwingMisc.java
@@ -0,0 +1,15 @@
+package krythos.util.swing;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.Window;
+
+public class SwingMisc {
+	
+	public static void centerWindow(Window frame) {
+	    Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+	    int x = (int) ((dimension.getWidth() - frame.getWidth()) / 2);
+	    int y = (int) ((dimension.getHeight() - frame.getHeight()) / 2);
+	    frame.setLocation(x, y);
+	}
+}
